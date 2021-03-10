8c190d947422708389b2e0e7bba749bb4b7821de
Anton <aann1h1latorr@gmail.com>
2021-03-10 18:24:59 +0700

    Added button to hide finished todo's

--- src/TodoPanel.tsx
+++ src/TodoPanel.tsx
@@ -14,6 +14,12 @@ function TodoPanel() {
 
   const [input, setInput] = useState<string>("");
 
+  const [hideFinished, setHideFinished] = useState<boolean>(false);
+
+  const toggleHideFinished = () => {
+    setHideFinished(!hideFinished);
+  };
+
   const handleChange = (event: any) => {
     setInput(event.target.value);
   };
@@ -43,7 +49,7 @@ function TodoPanel() {
     <div className="flex items-center bg-image bg-cover justify-center h-screen">
       <div className="font-sans flex-col text-white flex items-center bg-indigo-400 bg-opacity-10 border shadow rounded-lg p-3 w-1/3 h-auto max-h-2/3">
         <h1 className="p-5 text-lg font-bold">Todo App</h1>
-        <form action="" className="w-3/4 mb-10">
+        <form action="" className="w-3/4 mb-5">
           <div className="text-black flex items-center bg-white rounded-lg mb-2">
             <label className="w-10 text-right mr-8">Title:</label>
             <input
@@ -63,15 +69,41 @@ function TodoPanel() {
             Add
           </button>
         </form>
+        {hideFinished ? (
+          <button
+            className="block focus:outline-none text-black w-3/4 rounded bg-blue-200 text-white bg-opacity-20 p-2 mb-5"
+            onClick={toggleHideFinished}
+          >
+            Hide Finished
+          </button>
+        ) : (
+          <button
+            className="block focus:outline-none text-black w-3/4 rounded bg-white p-2 mb-5"
+            onClick={toggleHideFinished}
+          >
+            Hide Finished
+          </button>
+        )}
+
         {todos.length ? <div className="w-full bg-white h-1"></div> : null}
         <div className="flex flex-col divide-y w-full  overflow-y-scroll">
-          {todos.map((item) => (
-            <Todo
-              todo={item}
-              finishTodo={finishTodo}
-              deleteTodo={deleteTodo}
-            ></Todo>
-          ))}
+          {todos.map((item) =>
+            hideFinished ? (
+              !item.finished ? (
+                <Todo
+                  todo={item}
+                  finishTodo={finishTodo}
+                  deleteTodo={deleteTodo}
+                ></Todo>
+              ) : null
+            ) : (
+              <Todo
+                todo={item}
+                finishTodo={finishTodo}
+                deleteTodo={deleteTodo}
+              ></Todo>
+            )
+          )}
         </div>
       </div>
     </div>
