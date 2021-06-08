cd16e2ed8d8b4cc3a0674c71109013c4320ea0ff
Anton <aann1h1latorr@gmail.com>
2021-06-08 08:44:04 +0700

    Moved select bar

--- src/TodoPanel.tsx
+++ src/TodoPanel.tsx
@@ -24,7 +24,7 @@ const sortOptions: ISortOption[] = [
       if (first.dateAdded < second.dateAdded) return -1;
       return 0;
     },
-    name: "By Date",
+    name: "Date",
   },
   {
     id: 2,
@@ -33,7 +33,7 @@ const sortOptions: ISortOption[] = [
       if (first.title < second.title) return -1;
       return 0;
     },
-    name: "By Title",
+    name: "Title",
   },
 ];
 
@@ -89,61 +89,6 @@ const TodoPanel = () => {
 
   return (
     <div className="flex items-center bg-image bg-cover justify-center h-screen">
-      <div className="w-72 fixed top-60">
-        <Listbox value={selectedSortOption} onChange={setSelectedSortOption}>
-          <Listbox.Label className="text-white">Sort: </Listbox.Label>
-          <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
-            <span className="block truncate">{selectedSortOption.name}</span>
-            <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-              <SelectorIcon
-                className="w-5 h-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </span>
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            leave="transition ease-in duration-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {sortOptions.map((item: ISortOption) => (
-                <Listbox.Option
-                  className={({ active }) =>
-                    `${active ? "text-amber-900 bg-gray-100" : "text-gray-900"}
-                        cursor-default select-none relative py-2 pl-10 pr-4`
-                  }
-                  key={item.id}
-                  value={item}
-                >
-                  {({ selected, active }) => (
-                    <>
-                      <span
-                        className={`${
-                          selected ? "font-medium" : "font-normal"
-                        } block truncate`}
-                      >
-                        {item.name}
-                      </span>
-                      {selected ? (
-                        <span
-                          className={`${
-                            active ? "text-amber-600" : "text-amber-600"
-                          }
-                                absolute inset-y-0 left-0 flex items-center pl-3`}
-                        >
-                          <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </Listbox>
-      </div>
       <div className="font-sans text-white flex-col flex items-center bg-indigo-400 bg-opacity-10 border shadow rounded-lg p-3 w-1/3 h-auto max-h-2/3">
         <h1 className="p-5 text-lg font-bold">Todo App</h1>
         <form action="" className="w-3/4 mb-5">
@@ -168,20 +113,79 @@ const TodoPanel = () => {
         </form>
         {hideFinished ? (
           <button
-            className="block focus:outline-none text-black w-3/4 rounded bg-blue-200 text-white bg-opacity-20 p-2 mb-5"
+            className="block focus:outline-none text-black w-3/4 rounded bg-blue-200 text-white bg-opacity-20 p-2"
             onClick={toggleHideFinished}
           >
             Hide Finished
           </button>
         ) : (
           <button
-            className="block focus:outline-none text-black w-3/4 rounded bg-white p-2 mb-5"
+            className="block focus:outline-none text-black w-3/4 rounded bg-white p-2"
             onClick={toggleHideFinished}
           >
             Hide Finished
           </button>
         )}
 
+        <div className="w-80 p-1">
+          <Listbox value={selectedSortOption} onChange={setSelectedSortOption}>
+            <div className="flex flex-row items-center">
+              <Listbox.Label className="text-white p-2">Sort by:</Listbox.Label>
+              <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-black text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                <span className="block truncate">
+                  {selectedSortOption.name}
+                </span>
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                  <SelectorIcon
+                    className="w-5 h-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                </span>
+              </Listbox.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute w-80 py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {sortOptions.map((item: ISortOption) => (
+                  <Listbox.Option
+                    className={({ active }) =>
+                      `${active ? "text-black bg-gray-100" : "text-gray-900"}
+                        cursor-default select-none relative py-2 pl-10 pr-4`
+                    }
+                    key={item.id}
+                    value={item}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`${
+                            selected ? "font-medium" : "font-normal"
+                          } block truncate`}
+                        >
+                          {item.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`${
+                              active ? "text-amber-600" : "text-amber-600"
+                            }
+                                absolute inset-y-0 left-0 flex items-center pl-3`}
+                          >
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </Listbox>
+        </div>
         {todos.length ? <div className="w-full bg-white h-1"></div> : null}
         <div className="flex flex-col divide-y w-full  overflow-y-scroll">
           {todos.map((item) =>
